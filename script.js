b3faddc3350669cf86fc74d97607331215bb68ab
Humayun Khan <humayunk.pvt@gmail.com>
2023-02-19 23:01:24 +0530

    init

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var clothingCards = document.getElementById("clothContainer");
-
+// --------- CREATING CLOTHING CARDS USING JAVASCRIPT ---------
+const clothingCards = document.getElementById("clothContainer");
 const cards = [
   {
     id: 1,
@@ -38,6 +38,7 @@ const cards = [
   },
 ];
 
+// --------- FUNCTION TO CREATE CLOTHING CARDS ---------
 let output = "";
 for (let i = 0; i < cards.length; i++) {
 output += `<div class="card" id="${cards[i].id}">
@@ -51,3 +52,59 @@ output += `<div class="card" id="${cards[i].id}">
 }
 
 clothingCards.innerHTML = output;
+
+
+// --------- CREATING ACCESSORIES CARDS USING JAVASCRIPT ---------
+const accessoryContainer = document.getElementById("accessoryContainer");
+const cards2 = [
+  {
+    id: 6,
+    img: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/9803247/2019/5/27/624dac60-4c42-4902-bba1-30a51cc7f43c1558948536543-Apple-Unisex-Smart-Watches-1441558948536225-1.jpg",
+    title: "Unisex Silver-Toned Series 3 Smart Watch",
+    subtitle: "Apple",
+    price: "Rs 31999"
+  },
+  {
+    id: 7,
+    img: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/productimage/2019/2/23/dc05b5e5-aa82-493e-bf0b-0c309baf21021550920383101-1.jpg",
+    title: "Unisex Black & Green Reflex 2.0 Smart Band",
+    subtitle: "Fastrack",
+    price: "Rs 1999"
+  },
+  {
+    id: 8,
+    img: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/10122083/2019/6/24/e533a691-3908-41b0-8307-1928a37d4ec41561362162650-Samsung-Galaxy-Fit-4801561362161527-1.jpg",
+    title: "Unisex Black Galaxy Fit Fitness Band",
+    subtitle: "Samsung",
+    price: "Rs 9990"
+  },
+  {
+    id: 9,
+    img: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/8593217/2019/4/9/de2c21f3-6731-4c29-9369-692a486a1b8c1554802772926-Samsung-Gear-IconX-Black-Cord-free-Fitness-Earbuds-256155480-1.jpg",
+    title: "Gear IconX Black Cord-free Fitness Earbuds",
+    subtitle: "Samsung",
+    price: "Rs 13990"
+  },
+  {
+    id: 10,
+    img: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/9803279/2019/5/27/6661d579-12ae-456b-b8f2-e78e5256a99c1558948436173-Apple-AirPods-with-Charging-Case-2nd-Gen-White-3831558948435-1.jpg",
+    title: "White 2nd Gen AirPods with Charging Case",
+    subtitle: "Apple",
+    price: "Rs 14999"
+  },
+];
+
+// --------- FUNCTION TO CREATE CLOTHING CARDS ---------
+let output2 = "";
+for (let i = 0; i < cards2.length; i++) {
+output2 += `<div class="card" id="${cards2[i].id}">
+    <div class="img"><img src="${cards2[i].img}"></div>
+    <div class="details">
+        <h3>${cards2[i].title}</h3>
+        <h4>${cards2[i].subtitle}</h4>
+        <h5>${cards2[i].price}</h5>
+    </div>
+</div>`
+}
+
+accessoryContainer.innerHTML = output2;
